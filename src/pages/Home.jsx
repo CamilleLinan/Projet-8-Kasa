560639148293dba04e1c84757724693ed9afcefb
Camille Linan <camille.linan@hotmail.com>
2022-11-07 02:34:18 +0100

    home page OK

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Layout/Header"
 import Banner from "../components/Home/Banner"
 import DisplayHouses from "../components/Home/DisplayHousings"
+import Footer from "../components/Layout/Footer"
 
 const Home = () => {
     return(
@@ -8,6 +9,7 @@ const Home = () => {
             <Header />
             <Banner />
             <DisplayHouses />
+            <Footer />
         </>
     )
 }
